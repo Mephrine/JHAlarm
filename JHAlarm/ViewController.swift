b9d82f79c56a76d5cd7fd2c8f3bf16dd458ad68b
KimJaehyun <jhkim01@adcapsule.co.kr>
2019-08-12 16:01:54 +0900

    Initial Commit

---
+++ JHAlarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JHAlarm
+//
+//  Created by 김제현 on 12/08/2019.
+//  Copyright © 2019 김제현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
